1d5698fd38ba81640cbfebaa08e0bed40c0917bb
Nayem <nayemsb12@gmail.com>
2026-07-21 15:42:52 +0600

    variables and data types

---
+++ basics/variables.java
@@ -0,0 +1,38 @@
+public class variables {
+    public static void main(String[] args) {
+
+        String name = "Nayem";
+        int age = 25;
+        double price = 25.25;
+        int a = 25;
+        int b = 4;
+
+        b = 34;
+        name = "Nayem Nobita";
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(a+b);
+        System.out.println(price);
+
+    }
+}
+
+// Data types
+/* 
+
+    Primitive                   Non-Primitive
+
+    byte                        String
+    short                       Array
+    char                        Class
+    boolean                     Object
+    int                         Interface
+    long
+    flaot
+    double
+
+
+
+*/
