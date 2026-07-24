8aa7d248e90fd0cf44b2cb1c494773a8909d6d6b
Nayem <nayemsb12@gmail.com>
2026-07-25 01:32:57 +0600

    else if statement

---
+++ basics/else_if.java
@@ -0,0 +1,29 @@
+public class else_if {
+    public static void main(String[] args) {
+        int weather = 2; // 1 = raining, 2 = sunny, 3 = cloudy
+
+        if (weather == 1) {
+        System.out.println("Bring an umbrella.");
+        } else if (weather == 2) {
+        System.out.println("Wear sunglasses.");
+        } else {
+        System.out.println("Just go outside normally.");
+        }
+
+
+
+
+        int time = 16;
+
+        if (time < 12) {
+        System.out.println("Good morning.");
+        } else if (time < 18) {
+        System.out.println("Good day.");
+        } else {
+        System.out.println("Good evening.");
+        }
+
+
+        
+    }
+}
