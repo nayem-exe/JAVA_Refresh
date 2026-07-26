99aec4e7606fa7561c09721d0c550ac7bc0ffe51
Nayem <nayemsb12@gmail.com>
2026-07-26 12:47:54 +0600

    even  odd

---
+++ basics/even_odd.java
@@ -0,0 +1,11 @@
+public class even_odd {
+    public static void main(String[] args) {
+        int myNum = 5;
+
+        if (myNum % 2 == 0){
+            System.out.println(myNum + " number is even.");
+        }else{
+            System.out.println(myNum + " number is odd");
+        }
+    }
+}
