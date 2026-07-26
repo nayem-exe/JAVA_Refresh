6f6f1d1c40502f6172e008a0d32294288bc35c3f
Nayem <nayemsb12@gmail.com>
2026-07-26 13:03:33 +0600

    check temparature

---
+++ basics/check_temparature.java
@@ -0,0 +1,13 @@
+public class check_temparature {
+    public static void main(String[] args) {
+        int temparature = 10;
+
+        if (temparature < 0){
+            System.out.println("It's freezing.");
+        }else if (temparature < 20){
+            System.out.println("It's cold.");
+        }else{
+            System.out.println("It's warm.");
+        }
+    }
+}
