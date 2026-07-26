0383df2cf1d6174def2842a781723aba4f5c71be
Nayem <nayemsb12@gmail.com>
2026-07-26 16:44:58 +0600

    do while loop

---
+++ basic_level2/do_while.java
@@ -0,0 +1,18 @@
+public class do_while {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while(i < 5);
+
+        int j = 10;
+
+        do {
+        System.out.println("j is " + j);
+        j++;
+        } while (j < 5);
+    }
+}
---
+++ basic_level2/do_while_examples.java
@@ -0,0 +1,13 @@
+public class do_while_examples {
+    public static void main(String[] args) {
+        int dice = 2;
+
+        while(dice <= 6){
+            if (dice < 6){
+                System.out.println("Yatzi");
+            }else{
+                System.out.println("Not Yatzi");
+            } dice++;
+        }
+    }
+}
