073e098dedeac21a458241621d4d77891bff51b0
Nayem <nayemsb12@gmail.com>
2026-07-26 08:55:49 +0600

    a real life example added

--- basics/nested_if.java
+++ basics/nested_if.java
@@ -11,6 +11,22 @@ public class nested_if {
             }
         }
 
+        int age = 20;
+        boolean isCitizen = true;
+
+        if(age >= 18) {
+            System.out.println("Old enough to vote.");
+
+            if (isCitizen) {
+                System.out.println("And you are a citizen, so you can vote.");
+
+            }else{
+                System.out.println("You are not a citizen, so you can't vote.");
+            }
+        }else{
+            System.out.println("Not old enough to vote.");
+        }
+
     
     }
 }
