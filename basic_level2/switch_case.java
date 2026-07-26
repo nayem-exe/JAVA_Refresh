c2b840d695ced6ef5570f347be779a9bc0ac629e
Nayem <nayemsb12@gmail.com>
2026-07-26 14:26:04 +0600

    swithch case

---
+++ basic_level2/switch_case.java
@@ -0,0 +1,40 @@
+public class switch_case {
+    public static void main(String[] args) {
+        int day = 4;
+        switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+        }
+
+        //Another way
+        int day2 = 3;
+        switch (day2) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+        }
+    }
+}
